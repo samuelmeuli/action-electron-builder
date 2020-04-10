c49b5dafa9c9e4c86dc1c66062a828f7c1aab6f5
Matthew Rathbone <matthew.rathbone@gmail.com>
2020-04-10 12:59:26 -0500

    review feedback

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const exit = msg => {
  * Executes the provided shell command and redirects stdout/stderr to the console
  */
 const run = (cmd, cwd) => {
-  console.log(`+ ${cmd}`);
-  execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
+	execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
 }
 
 /**
@@ -71,8 +70,8 @@ const runAction = () => {
 	const release = getInput("release", true) === "true";
 	const pkgRoot = getInput("package_root", true);
 	const buildScriptName = getInput("build_script_name", true);
-  const skipBuild = getInput("skip_build") === "true"
-  const vueCliBuild = getInput("use_vue_cli") === "true";
+	const skipBuild = getInput("skip_build") === "true"
+	const useVueCli = getInput("use_vue_cli") === "true";
 
 	// TODO: Deprecated option, remove in v2.0. `electron-builder` always requires a `package.json` in
 	// the same directory as the Electron app, so the `package_root` option should be used instead
@@ -110,24 +109,24 @@ const runAction = () => {
 	run(useNpm ? "npm install" : "yarn", pkgRoot);
 
 	// Run NPM build script if it exists
-  if (!skipBuild) {
-    log("Running the build script…");
-    if (useNpm) {
-      run(`npm run ${buildScriptName} --if-present`, pkgRoot);
-    } else {
-      // TODO: Use `yarn run ${buildScriptName} --if-present` once supported
-      // https://github.com/yarnpkg/yarn/issues/6894
-      const pkgJson = JSON.parse(readFileSync(pkgJsonPath, "utf8"));
-      if (pkgJson.scripts && pkgJson.scripts[buildScriptName]) {
-        run(`yarn run ${buildScriptName}`, pkgRoot);
-      }
-    }
-  } else {
-    log("Skipping build script because `skip_build` option is set")
-  }
+	if (skipBuild) {
+		log("Skipping build script because `skip_build` option is set")
+	} else {
+		log("Running the build script…");
+		if (useNpm) {
+			run(`npm run ${buildScriptName} --if-present`, pkgRoot);
+		} else {
+			// TODO: Use `yarn run ${buildScriptName} --if-present` once supported
+			// https://github.com/yarnpkg/yarn/issues/6894
+			const pkgJson = JSON.parse(readFileSync(pkgJsonPath, "utf8"));
+			if (pkgJson.scripts && pkgJson.scripts[buildScriptName]) {
+				run(`yarn run ${buildScriptName}`, pkgRoot);
+			}
+		}
+	}
 
 	log(`Building${release ? " and releasing" : ""} the Electron app…`);
-  const cmd = vueCliBuild ? "electron:build" : "electron-builder";
+	const cmd = useVueCli ? "vue-cli-service electron:build" : "electron-builder";
 	run(
 		`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
 			release ? "--publish always" : ""
