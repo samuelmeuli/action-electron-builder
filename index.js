aa22db776492f988a7f8af154e990bda8dade6e3
Julian Gruber <julian@juliangruber.com>
2023-02-06 22:50:42 +0100

    forgot --no-install

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ const runAction = () => {
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
-				`${useNpm ? "npx" : "yarn run"} ${cmd} --${platform} ${
+				`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
 					release ? "--publish always" : ""
 				} ${args}`,
 				appRoot,
